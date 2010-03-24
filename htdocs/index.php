58176ed211c229c9a7df79e3cfcb7bc6ff472d3f
snivelus <snivelus@7830a86a-2778-11df-a94c-d58e26249b9a>
2010-03-24 16:37:57 +0000

    Updated index page for new database schema

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,23 +4,69 @@
     <p>Welcome to <span class="special">iCafeteria</span>.</p>
 </div>
 
-<h2>Our Menu</h2>
-
 <ul class="menu">
 <?php
-$results = mysql_query('select items.*, ifnull(req.calories, 0) as req_calories, ifnull(req.calories, 0) + ifnull(opt.calories, 0) as opt_calories from items left join (select items.item_id, sum(items_ingred.calories) as calories from items join items_ingred on items.item_id = items_ingred.item_id group by items.item_id) as req on items.item_id = req.item_id left join (select items.item_id, sum(items_ingred_opt.calories) as calories from items join items_ingred_opt on items.item_id = items_ingred_opt.item_id group by items.item_id) as opt on items.item_id = opt.item_id', $db)
+$results = mysql_query(<<<SQL
+SELECT items.*,
+       item_type_name,
+       req_ingreds.calories AS min_calories,
+       all_ingreds.calories AS max_calories
+  FROM       items
+        JOIN item_types
+       USING (item_type_id)
+        LEFT
+        JOIN (   SELECT items.item_id AS item_id, SUM(ingreds.calories) AS calories
+                   FROM       items
+                         JOIN items_ingreds
+                        USING (item_id)
+                         JOIN ingreds
+                        USING (ingred_id)
+                  WHERE NOT items_ingreds.optional
+               GROUP BY items.item_id
+             ) AS req_ingreds
+       USING (item_id)
+        LEFT
+        JOIN (   SELECT items.item_id AS item_id, SUM(ingreds.calories) AS calories
+                   FROM       items
+                         JOIN items_ingreds
+                        USING (item_id)
+                         JOIN ingreds
+                        USING (ingred_id)
+               GROUP BY items.item_id
+             ) AS all_ingreds
+       USING (item_id);
+SQL
+    , $db)
     or die('Select failed: ' . mysql_error());
 
+$prev_item_type_id = 0;
 while (($row = mysql_fetch_assoc($results)) !== FALSE) {
     $cost = money_format('$%n', $row['cost']);
 
-    echo '<li>';
-    echo "<div class=\"title\">$row[item_name] <span class=\"cost\">&mdash; $cost</span></div>";
+    if ($row['item_type_id'] != $prev_item_type_id) {
+        echo "<li><h3>$row[item_type_name]</h3></li>";
+        $prev_item_type_id = $row['item_type_id'];
+    }
 
-    $ingred_result = mysql_query("SELECT description FROM items_ingred WHERE items_ingred.item_id = $row[item_id]", $db)
-        or die ('Select failed'.mysql_error());
+    echo '<li>';
+    echo "<div class=\"title\">$row[item_name] <span class=\"cost\">&mdash; $cost</span>";
+
+    if(isset($_SESSION['username'])) {
+        $user_result = mysql_query("SELECT * FROM users WHERE username='$_SESSION[username]'", $db)
+            or die('Select failed: ' . mysql_error());
+        $user_row = mysql_fetch_assoc($user_result);
+
+        if ($user_row['user_type_id'] == 2 || $user_row['user_type_id'] == 3) {
+            echo "<span class=\"add-to-cart\"><a href=\"/manage/add_cart.php?item_id=$row[item_id]\">[add to cart]</a></span>";
+        }
+    }
+
+    echo "</div>";
 
     echo '<div class="ingrediants">';
+
+    $ingred_result = mysql_query("SELECT description FROM items_ingreds join ingreds using (ingred_id) WHERE items_ingreds.item_id = $row[item_id] AND items_ingreds.optional = FALSE", $db)
+        or die ('Select failed'.mysql_error());
 
     $first = true;
     while (($ingred_row = mysql_fetch_assoc($ingred_result)) != FALSE) {
@@ -28,9 +74,21 @@ while (($row = mysql_fetch_assoc($results)) !== FALSE) {
         $first = false;
     }
 
-    $calories = "$row[req_calories]&ndash;$row[opt_calories]";
+    echo '</div>';
+    echo '<div class="ingrediants">';
+
+    $ingred_opt_result = mysql_query("SELECT description FROM items_ingreds join ingreds using (ingred_id) WHERE items_ingreds.item_id = $row[item_id] AND items_ingreds.optional = TRUE", $db)
+        or die ('Select failed'.mysql_error());
+
+    $first = true;
+    while (($ingred_opt_row = mysql_fetch_assoc($ingred_opt_result)) != FALSE) {
+        echo ($first ? 'Optional Ingrediants: ' : ', ') . $ingred_opt_row['description'];
+        $first = false;
+    }
 
     echo '</div>';
+
+    $calories = "$row[min_calories]&ndash;$row[max_calories]";
     echo "<div class=\"calories\">$calories cal</div>";
     echo '</li>';
 }
