87546a473b04d61e0bb2fe4c60c45872637aae66
snivelus <snivelus@7830a86a-2778-11df-a94c-d58e26249b9a>
2010-04-27 06:40:14 +0000

    Added skeleton page for managing orders. This time I actually included the page.

---
+++ htdocs/manage/manage.php
@@ -0,0 +1,15 @@
+<?php include('../../include/header.php'); ?>
+
+<?php
+switch ($_POST['action']) {
+}
+?>
+
+<div id="welcome">
+    <p>Welcome to <span class="special">iCafeteria</span>.</p>
+</div>
+
+<h2>Manage Orders:</h2>
+
+<?php include('../../include/footer.php'); ?>
+
