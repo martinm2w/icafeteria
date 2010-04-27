0d7216215274caa6a83359fa8e48a81e756c9b17
snivelus <snivelus@7830a86a-2778-11df-a94c-d58e26249b9a>
2010-04-27 09:24:41 +0000

    Manager can view orders, print receipts and labels, and mark orders paid.

---
+++ trunk/htdocs/manage/label.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+$db = mysql_connect('localhost:3306', 'www') or die('Cannot connect to database: ' . mysql_error());
+
+if ($_SERVER['SERVER_NAME'] == 'icafeteria.linuxbox') {
+    mysql_select_db('icafeteria_local', $db) or die('Database switch failed: ' . mysql_error());
+}
+else {
+    mysql_select_db('icafeteria', $db) or die('Database switch failed: ' . mysql_error());
+}
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>iCafeteria</title>
+    </head>
+    <body>
+
+        <ul class="menu">
+<?php
+$total_results = mysql_query(<<<SQL
+SELECT order_id,
+       f_name,
+       l_name,
+       SUM(count * cost) AS subtotal_cost,
+       SUM(count * cost) * 0.08 AS total_tax,
+       SUM(count * cost) * 1.08 AS total_cost_with_tax,
+       NOW() + INTERVAL MAX(prep_time) MINUTE AS min_pickup_time,
+       NOW() + INTERVAL SUM(count * prep_time) MINUTE AS max_pickup_time
+  FROM       orders
+        JOIN orders_items
+       USING (order_id)
+        JOIN items
+       USING (item_id)
+        JOIN users
+       USING (user_id)
+ WHERE order_id = $_GET[order_id]
+SQL
+    , $db)
+    or die('Select failed: ' . mysql_error());
+
+$results = mysql_query(<<<SQL
+SELECT order_id,
+       count,
+       items.*
+  FROM       orders
+        JOIN orders_items
+       USING (order_id)
+        JOIN items
+       USING (item_id)
+ WHERE order_id = $_GET[order_id]
+SQL
+    , $db)
+    or die('Select failed: ' . mysql_error());
+
+if (!mysql_num_rows($results)) {
+}
+else {
+    $total_row = mysql_fetch_assoc($total_results);
+    $prep_time = $total_row['min_pickup_time'];
+
+    if ($total_row['min_pickup_time'] != $total_row['max_pickup_time']) {
+        $prep_time .= "&mdash;$total_row[max_pickup_time]";
+    }
+
+    echo '<h1>Order #' . str_pad($total_row['order_id'], 5, '0', STR_PAD_LEFT) . '</h1>';
+    echo "<h3>$total_row[f_name] $total_row[l_name]</h3>";
+    echo "<p>Expected pickup time: $prep_time</p>";
+    echo '<p>&nbsp;</p>';
+
+    while (($row = mysql_fetch_assoc($results)) !== FALSE) {
+        $cost = money_format('$%n', $row['cost']);
+
+        echo '<li>';
+        echo "<div class=\"title\">$row[count]x $row[item_name] <span class=\"cost\">@ $cost each</span></div>";
+
+        echo '<div class="ingrediants">';
+
+        $ingred_opt_result = mysql_query("SELECT description FROM orders_items_ingreds JOIN ingreds USING (ingred_id) JOIN items_ingreds USING (item_id, ingred_id) WHERE orders_items_ingreds.item_id = $row[item_id] AND orders_items_ingreds.order_id = $row[order_id] AND optional = TRUE", $db)
+            or die ('Select failed'.mysql_error());
+
+        $first = true;
+        while (($ingred_opt_row = mysql_fetch_assoc($ingred_opt_result)) != FALSE) {
+            echo ($first ? 'Optional Ingrediants: ' : ', ') . $ingred_opt_row['description'];
+            $first = false;
+        }
+
+        echo '</div>';
+        echo '</li>';
+    }
+
+    $subtotal_cost = money_format('$%n', $total_row['subtotal_cost']);
+    $total_tax = money_format('$%n', $total_row['total_tax']);
+    $total_cost_with_tax = money_format('$%n', $total_row['total_cost_with_tax']);
+
+    echo '<p>&nbsp;</p>';
+    echo "<p>Subtotal: $subtotal_cost</p>";
+    echo "<p>Tax (8.00%): $total_tax</p>";
+    echo "<h3>Total: $total_cost_with_tax</h3>";
+}
+?>
+    </body>
+</html>
+
--- trunk/htdocs/manage/manage.php
+++ trunk/htdocs/manage/manage.php
@@ -2,6 +2,16 @@
 
 <?php
 switch ($_POST['action']) {
+case 'pay':
+    mysql_query(<<<SQL
+UPDATE orders
+   SET paid = TRUE
+ WHERE order_id = $_POST[order_id]
+SQL
+        , $db)
+        or die('Update failed: ' . mysql_error());
+    echo "<p>An order was marked as paid.</p>";
+    break;
 }
 ?>
 
@@ -10,6 +20,49 @@ switch ($_POST['action']) {
 </div>
 
 <h2>Manage Orders:</h2>
+
+<?php
+$results = mysql_query(<<<SQL
+SELECT orders.order_id,
+       CONCAT(users.f_name, ' ', users.l_name) AS name,
+       ROUND(cost, 2) AS cost
+  FROM       orders
+        JOIN ( SELECT order_id,
+                      SUM(count * cost) AS cost
+                 FROM       orders
+                       JOIN orders_items
+                      USING (order_id)
+                       JOIN items
+                      USING (item_id)
+                GROUP
+                   BY order_id
+             ) AS orders_cost
+       USING (order_id)
+        JOIN users
+       USING (user_id)
+ WHERE     completed
+       AND NOT paid
+ ORDER
+    BY order_time
+SQL
+    , $db)
+    or die('Select failed: ' . mysql_error());
+?>
+
+<table width="100%">
+    <thead>
+        <tr><th>Order ID</th><th>Name</th><th>Total</th><th><th><th></th></tr>
+    </thead>
+    <tfoot>
+    </tfoot>
+    <tbody>
+<?php
+while (($row = mysql_fetch_assoc($results)) !== FALSE) {
+    echo "<tr><td>" . str_pad($row['order_id'], 5, '0', STR_PAD_LEFT) . "</td><td>$row[name]</td><td>$$row[cost]</td><td><a href=\"label.php?order_id=$row[order_id]\" target=\"_blank\">[Print Label]</a></td><td><a href=\"receipt.php?order_id=$row[order_id]\" target=\"_blank\">[Print Receipt]</a></td><td><form action=\"manage.php\" method=\"POST\"><input type=\"hidden\" name=\"order_id\" value=\"$row[order_id]\"><button type=\"submit\" name=\"action\" value=\"pay\">Mark Paid</button></form></td></tr>";
+}
+?>
+    </tbody>
+</table>
 
 <?php include('../../include/footer.php'); ?>
 
